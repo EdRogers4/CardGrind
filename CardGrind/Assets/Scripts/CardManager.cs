8185f7481aae07e9c58a0f45a9539a4461960e06
EdRogers4 <59186908+EdRogers4@users.noreply.github.com>
2023-01-03 20:29:22 -0500

    Added variables

--- CardGrind/Assets/Scripts/CardManager.cs
+++ CardGrind/Assets/Scripts/CardManager.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour
 {
     [SerializeField] private Button[] _playerCardButton;
+    [SerializeField] private TextMeshProUGUI[] _textDamagePlayer;
+    [SerializeField] private TextMeshProUGUI[] _textDamageEnemy;
+    [SerializeField] private int[] _damagePlayer;
+    [SerializeField] private int[] _damageEnemy;
+    
     [SerializeField] private Image _highlightedBorder;
+
     
     void Start()
     {
